637b6f5eef5a3bb59e315ca530e0140e9ab9765e
Md. Khayrul Islam <devkhayrul@gmail.com>
2021-07-27 15:52:13 +0600

    display name changed

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -9,7 +9,7 @@ const Profile = () => {
     }
     return (
         <div className="profile">
-            <h3 contentEditable='true'>Name:{loggedInUser.displayName}</h3>
+            <h3 contentEditable='true'>Name:{loggedInUser.name}</h3>
             <p>Email:{loggedInUser.email}</p>
             <button onClick={handleLogOut}>Log out</button>
         </div>
